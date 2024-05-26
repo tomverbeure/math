cdf4ffbb97bd7ef7c2efa92f536a513a7923ed43
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2024-05-26 20:24:43 +0200

    Add StageMask utility function

--- src/main/scala/math/Misc.scala
+++ src/main/scala/math/Misc.scala
@@ -1,4 +1,3 @@
-
 package math
 
 import spinal.core._
@@ -6,6 +5,7 @@ import spinal.core.sim._
 
 import scala.collection.mutable
 import spinal.lib.sim.Phase
+import spinal.lib.misc.pipeline.StageLink
 
 object LeadingZeros {
 
@@ -13,38 +13,39 @@ object LeadingZeros {
     // https://electronics.stackexchange.com/questions/196914/verilog-synthesize-high-speed-leading-zero-count
     // Code by @typingArtist on SpinalHDL gitter channel: https://gitter.im/SpinalHDL/SpinalHDL?at=5bbe075e435c2a518e81dd83
 
-    def apply(input: Bits): UInt = calcOnes(~input).resize(log2Up(input.getWidth+1))
+    def apply(input: Bits): UInt = calcOnes(~input).resize(log2Up(input.getWidth + 1))
 
     def calcOnes(input: Bits): UInt = input.getWidth match {
         case 0 => U""
         case 1 => input.asUInt
         case a => {
-            val leftBits = 1 << (log2Up(a)-1)
-            val upper = calcOnes(input.resizeLeft(leftBits))
-            val lower = calcOnes(input.resize(a - leftBits)).resize(upper.getWidth)
+            val leftBits = 1 << (log2Up(a) - 1)
+            val upper    = calcOnes(input.resizeLeft(leftBits))
+            val lower    = calcOnes(input.resize(a - leftBits)).resize(upper.getWidth)
             (upper.msb ## lower.msb).mux(
-                B"11"   -> U"10" @@ upper.resize(upper.getWidth-1),
-                B"10"   -> U"01" @@ lower.resize(lower.getWidth-1),
-                default -> U"00" @@ upper.resize(upper.getWidth-1)
-                )
+              B"11"   -> U"10" @@ upper.resize(upper.getWidth - 1),
+              B"10"   -> U"01" @@ lower.resize(lower.getWidth - 1),
+              default -> U"00" @@ upper.resize(upper.getWidth - 1)
+            )
         }
     }
 
 }
 
 object OptPipe {
-    def apply[T <: Data](that : T, ena: Bool, pipeline : Boolean) : T = if (pipeline) RegNextWhen(that, ena) else that
-    def apply[T <: Data](that : T, pipeline : Boolean) : T = apply(that, True, pipeline)
+    def apply[T <: Data](that: T, ena: Bool, pipeline: Boolean): T = if (pipeline) RegNextWhen(that, ena) else that
+    def apply[T <: Data](that: T, pipeline: Boolean): T            = apply(that, True, pipeline)
 }
 
 object OptPipeInit {
-    def apply[T <: Data](that : T, init: T, ena: Bool, pipeline : Boolean) : T = if (pipeline) RegNextWhen(that, ena) init(init) else that
-    def apply[T <: Data](that : T, init: T, pipeline : Boolean) : T = apply(that, init, True, pipeline)
+    def apply[T <: Data](that: T, init: T, ena: Bool, pipeline: Boolean): T =
+        if (pipeline) RegNextWhen(that, ena) init (init) else that
+    def apply[T <: Data](that: T, init: T, pipeline: Boolean): T = apply(that, init, True, pipeline)
 }
 
 case class ScoreboardInOrder[T]() {
-    val dut = mutable.Queue[T]()
-    val ref = mutable.Queue[(T, Any)]()
+    val dut     = mutable.Queue[T]()
+    val ref     = mutable.Queue[(T, Any)]()
     var matches = 0
 
     if (Phase.isUsed) {
@@ -67,7 +68,7 @@ case class ScoreboardInOrder[T]() {
 
     def check(): Unit = {
         if (ref.nonEmpty && dut.nonEmpty) {
-            val dutHead = dut.dequeue()
+            val dutHead           = dut.dequeue()
             val (refHead, inputs) = ref.dequeue()
             if (!compare(refHead, dutHead)) {
                 println("Transaction mismatch :")
@@ -126,7 +127,7 @@ object BundleDebug {
 
                 ds match {
                     case d: Data => recurse(d)
-                    case _ => {}
+                    case _       => {}
                 }
             }
             c.rework {
@@ -149,4 +150,53 @@ object BundleDebug {
           case _          => None
       }
     )
+}
+
+object StageMask {
+    /*
+     Size determines the number of stages and enableOrder contains the order at which each stage
+     will be enabled if an int is used to initialize the mask.
+
+     A config should be made an implicit parameter whenever a mask is used.
+     */
+    case class Config(size: Int, enableOrder: List[Int]) {
+        assert(enableOrder.length == size)
+        assert(enableOrder.distinct.length == size)
+        assert(enableOrder.forall(_ < size))
+    }
+
+    /* Converts an integer to a mask by looking at the enable order given */
+    implicit def int2StageMask(x: Int) = StageMask(Right(x))
+
+    implicit def list2StageMask(x: List[Boolean]) = StageMask(Left(x))
+}
+
+/* Indicates which stages should be active */
+case class StageMask(x: Either[List[Boolean], Int]) {
+    import spinal.lib.misc.pipeline.{Node, StageLink, DirectLink, Link}
+
+    def apply(i: Int)(implicit config: StageMask.Config) = mask()(config)(i)
+
+    def mask()(implicit config: StageMask.Config): List[Boolean] = x match {
+        case Right(x) => {
+            assert(x >= 0 && x <= config.size, f"x out of range for available pipeline stages ${config.size}")
+            List.tabulate(config.size)(i => config.enableOrder.slice(0, x).contains(i))
+        }
+        case Left(x) => {
+            assert(x.length == config.size)
+            x
+        }
+    }
+
+    def apply(nodes: Seq[Node])(implicit config: StageMask.Config): Seq[Link] = {
+        assert(nodes.length - 1 == mask.length)
+        nodes
+            .sliding(2)
+            .zip(mask.iterator)
+            .map { case (pair: Seq[Node], m: Boolean) =>
+                if (m) StageLink(pair(0), pair(1)) else DirectLink(pair(0), pair(1))
+            }
+            .toSeq
+    }
+
 }
